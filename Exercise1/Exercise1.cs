beccdbd8cc4fe8519d4ee06d22f08e61a22f6678
vdf <vasily.v.d@gmail.com>
2024-03-15 14:48:51 +0500

    Add files via upload

---
+++ Exercise1/Exercise1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Engine
+    {
+        private const int state_stopped = 1;
+        private const int state_started = 2;
+        public int engine_state;
+
+        public void start()
+        {
+            Console.Out.WriteLine( "Start" );
+            engine_state = state_stopped;
+        }
+
+        public void stop()
+        {
+            Console.Out.WriteLine( "Stop" );
+            engine_state = state_started;
+        }
+
+        public String getState()
+        {
+            String state = null;
+
+            if( engine_state == state_stopped )
+            {
+                state = "STOPPED";
+            }
+            else if( engine_state == state_started )
+            {
+                state = "STARTED";
+            }
+
+            return state;
+        }
+    }
+}
---
+++ Exercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Engine eng = new Engine();
+            eng.start();
+            Thread.Sleep( 2000 );
+            eng.stop();
+            Console.ReadLine();
+        }
+    }
+}
