07b3883e6dc9f3d04de855248515d69b10e56933
dima.shelest.12.12@mail.ru <dima.shelest.12.12@mail.ru>
2024-04-06 14:40:24 +0500

    fuel tank

---
+++ Exercise1/FuelTank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class FuelTank
+    {
+        private int capasity;
+        private int amound;
+        public FuelTank(int capasity)
+        {
+            this.capasity = capasity;
+        }
+        public void fill(int amound)
+        {
+         
+        }
+        public int Getamound()
+        {
+            return amound;
+        }
+        public int GetLevel()
+        {
+            
+        }
+    }
+}
